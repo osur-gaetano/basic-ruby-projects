7868c5badef86987b5aceed206cf7dac9b3e09b5
Gaetano Osur <165330888+osur-gaetano@users.noreply.github.com>
2024-04-02 11:03:39 +0300

    Implement ceaser_cipher function

---
+++ ceaser_cipher.rb
@@ -0,0 +1,13 @@
+def shift_character(character, shift)
+  return character unless character.match?(/[A-Za-z]/)
+  base_position = character.upcase == character ? "A".ord : "a".ord
+  ((character.ord - base_position + shift)%26 + base_position).chr
+end
+
+
+def ceaser_cipher(word, shift)
+  word.chars.map{ |character| shift_character(character, shift)}.join
+end
+
+
+ceaser_cipher("War Secrets to my generals", 5)
