6fa8565f0efe2021915a6047a18b0ef7f8c4b046
Gaetano Osur <165330888+osur-gaetano@users.noreply.github.com>
2025-02-05 14:04:41 +0300

    Refactor substrings method

--- sub_string.rb
+++ sub_string.rb
@@ -1,12 +1,9 @@
-dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+dictionary = %w[below down go going horn how howdy it i low own part partner sit]
 
-def substrings(string, dictionary)
-  word_count = Hash.new(0)
-  string_array = string.downcase.gsub(/[[:punct:]]/,'').split
-  dictionary.each do |dict_item|
-    string_array.each { |word| word_count[dict_item]+=1 if word.include? dict_item}
+def substrings(word, dictionary)
+  dictionary.each_with_object(Hash.new(0)) do |dictionary_word, substring_count|
+    substring_count[dictionary_word] += 1 if word.match? dictionary_word
   end
-  word_count
 end
 
 puts substrings("Howdy partner, sit down! How's it going?", dictionary)
