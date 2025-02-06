83e2c6b8891c79139917647cf7cc886858481178
Gaetano Osur <165330888+osur-gaetano@users.noreply.github.com>
2025-02-06 10:01:49 +0300

    Refactor bubble_sort method

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,17 +1,19 @@
 def bubble_sort(array)
-  iteration = array.length
-  (iteration).times do
-    swapped = true
-    for i in 1...iteration do
-      if array[i-1]> array[i]
-        array[i-1],array[i] = array[i],array[i-1]
+  array_length = array.length
+
+  loop do
+    swapped = false
+    for index in 1..array_length - 1 do
+      if array[index - 1] > array[index]
+        array[index - 1], array[index] = array[index], array[index - 1]
         swapped = true
       end
     end
-    iteration-=1
+    array_length -= 1
+    break unless swapped
   end
   array
 end
 
-array_list = [4,3,78,2,0,2,1,2]
+array_list = [4, 3, 78, 2, 0, 2, 1, 2]
 p bubble_sort(array_list)
